addfa3d5c7832e5db00a926a98670b4d5bfc89a8
Ulric Todman <ricksinclair.nyc@gmail.com>
2019-05-06 10:27:30 -0400

    made new dev branch and implemented state init with componentDidMount

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -1,49 +1,19 @@
 import React, { Component } from "react";
+import { returnMovies } from "../services/fakemovieservice";
 
-class Movies extends Component {
+export default class Movies extends Component {
   state = {
-    movies: [
-      {
-        title: "Die Hard",
-        genre: "Action",
-        stock: 10,
-        rate: 5
-      },
-      {
-        title: "Avengers: Infinity War",
-        genre: "Action",
-        stock: 15,
-        rate: 8
-      },
-      {
-        title: "The Notebook",
-        genre: "Romance",
-        stock: 12,
-        rate: 5
-      },
-      {
-        title: "The Lion King",
-        genre: "Family",
-        stock: 13,
-        rate: 5
-      },
-      {
-        title: "Avatar",
-        genre: "Sci-Fi",
-        stock: 10,
-        rate: 5
-      },
-      {
-        title: "Creed",
-        genre: "Sports Drama",
-        stock: 14,
-        rate: 5
-      }
-    ]
+    movies: []
   };
+  componentDidMount() {
+    this.setState({
+      movies: returnMovies()
+    });
+  }
 
   render() {
     let { movies } = this.state;
+
     return (
       <div>
         {/* 
@@ -82,5 +52,3 @@ class Movies extends Component {
     );
   }
 }
-
-export default Movies;
---
+++ src/services/fakemovieservice.js
@@ -0,0 +1,42 @@
+let moviesList = [
+  {
+    title: "Die Hard",
+    genre: "Action",
+    stock: 10,
+    rate: 5
+  },
+  {
+    title: "Avengers: Infinity War",
+    genre: "Action",
+    stock: 15,
+    rate: 8
+  },
+  {
+    title: "The Notebook",
+    genre: "Romance",
+    stock: 12,
+    rate: 5
+  },
+  {
+    title: "The Lion King",
+    genre: "Family",
+    stock: 13,
+    rate: 5
+  },
+  {
+    title: "Avatar",
+    genre: "Sci-Fi",
+    stock: 10,
+    rate: 5
+  },
+  {
+    title: "Creed",
+    genre: "Sports Drama",
+    stock: 14,
+    rate: 5
+  }
+];
+
+export const returnMovies = () => {
+  return moviesList;
+};
